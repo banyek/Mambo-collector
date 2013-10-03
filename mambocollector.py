f74b6eda6fbdf46608a8e2af5a52afad3e15eaf8
Balazs Pocze <banyek@gawker.com>
2013-10-03 18:11:37 +0200

    rewritten. Ready for production.

--- mambocollector.py
+++ mambocollector.py
@@ -1,106 +1,140 @@
-#!/usr/bin/python -u
-#
-# Statsd data collector for mysql
-# (c) Balazs Pocze <banyek@gmail.com>
-#
-import ConfigParser         # Handle config file
-import socket               # Send data to statsd
-import MySQLdb              # Query Mysql
-import threading            # To start Worker threads
-import time                 # To wait, log time etc.
-from daemon import runner   # Daemonize collector
+mport ConfigParser
+import threading
+import MySQLdb
+import sys
+import time
+import socket
+import os
+import Queue
+from daemon import runner
+from datetime import datetime
+
+class MySQLWorker(threading.Thread):
+
+	def __init__(self, mysqlconf, command, statsdsender, logger):
+		self.mysql_host = mysqlconf["mysql_host"][0]
+		self.mysql_user = mysqlconf["mysql_user"][0]
+		self.mysql_password = mysqlconf["mysql_password"][0]
+		self.mysql_database = mysqlconf["mysql_database"][0]
+		self.metricname = command["metricname"][0]
+		self.rate = float(command["rate"][0])
+		self.query = command["query"][0]
+		self.statsdsender = statsdsender
+		self.logger = logger
+		super(MySQLWorker, self).__init__()
+		self.logger.log("MySQLWorker created")
+
+	def run(self):
+		self.logger.log("MySQLWorker started")
+		while True:
+			try:
+				db = MySQLdb.connect(host=self.mysql_host,user=self.mysql_user,passwd=self.mysql_password,db=self.mysql_database)
+			except:
+                                print "Cannot connect to MySQL host"
+			dbcursor = db.cursor()
+			dbcursor.execute(self.query)
+			for rawdata in dbcursor.fetchone():
+				metricdata = self.metricname+":"+str(rawdata)+"|c"
+			self.statsdsender.send(metricdata)
+			self.logger.debuglog(metricdata)
+			db.close()
+			time.sleep(self.rate)
+
+class StatsdSender(object):
+
+	def __init__(self, statsdconf, logger):
+		self.statsd_host = statsdconf["statsd_host"][0]
+		self.statsd_port = int(statsdconf["statsd_port"][0])
+		super(StatsdSender, self).__init__()
+		self.logger = logger
+		self.logger.log("StatsdSender created")
+
+	def send(self, message):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.sendto(message, (self.statsd_host, self.statsd_port))
+
+class Mambo(object):
+
+	def __init__(self):
+		self.stdin_path = '/dev/null'
+		self.stdout_path = os.getenv('MAMBOLOG','/var/log/mambocollector.log')
+		self.stderr_path = os.getenv('MAMBOERR','/dev/tty')
+		self.pidfile_path = os.getenv('MAMBOPID','/var/run/mambocollector.pid')
+		self.configfile = os.getenv('MAMBOCFG','/etc/mambo/mambocollector.cnf')
+		self.commandfile = os.getenv('MAMBOCMD','/etc/mambo/mambocollector.cmd')
+		self.debug = os.getenv('MAMBODBG','0')
+		self.pidfile_timeout = 5
 
 
-class Worker(threading.Thread):
-    # Worker class (hero :-)
-    # Run in threaded mode, queries database for mysql checks, sends the key and the value to statsd
-    hostname = socket.gethostname()
-    mysql_host = ""
-    mysql_user = ""
-    mysql_password = ""
-    statsd_host = ""
-    statsd_port = ""
-    name = ""
-    rate = 0
-    query = ""
+	def configreader(self, config, section):
+		cnf = ConfigParser.RawConfigParser()
+		cnf.read(config)
+		configItems = cnf.items(section)
+		configuration = {}
+		for key, value in configItems:
+			configuration.setdefault(key, []).append(value)
+		return configuration
 
-    # Initializes worker thread, the connecton data is taken from config.cfg's [config] section, the others from per thread section
-    def __init__(self, mysql_host, mysql_user, mysql_password, statsd_host, statsd_port, name, rate, query):
-        self.mysql_host = mysql_host
-        self.mysql_user = mysql_user
-        self.mysql_password = mysql_password
-        self.name = name
-        self.rate = rate
-        self.query = query
-        self.statsd_host = statsd_host
-        self.statsd_port = statsd_port
-        super(Worker, self).__init__()
-        #print "Worker thread created!"
+	def commandreader(self, commandfile):
+		cmd = ConfigParser.RawConfigParser()
+		cmd.read(commandfile)
+		commands = cmd.sections()
+		return commands
 
-    # Connects database, run the query in every 'rate' seconds
+	def checkfile(self, file):
+		try:
+			with open(file): pass
+		except IOError:
+			logger.log("Can't open file","error")
+			exit(1)
 
-    def run(self):
-        while True:
-                db = MySQLdb.connect(host=self.mysql_host,     # your host, usually localhost
-                                     user=self.mysql_user,        # your username
-                                     passwd=self.mysql_password,  # your password
-                                     db="information_schema")     # name of the data base
-                cur = db.cursor()
-                cur.execute(self.query)
+	def run(self):
+		logger = Logger(self.debug)
+		logger.daemon = True
+		logger.start()
+		logger.log("Mambo started")
+		workers = []
+		self.checkfile(self.configfile)
+		statsdconf = self.configreader(self.configfile, 'statsd')
+		statsd = StatsdSender(statsdconf,logger)
+		mysqlconf = self.configreader(self.configfile, 'mysql')
+		commands = self.commandreader(self.commandfile)
+		for command in commands:
+			com = self.configreader(self.commandfile, command)
+			worker = MySQLWorker(mysqlconf, com, statsd, logger)
+			workers.append(worker)
+		for worker in workers:
+			worker.daemon = True
+			worker.start()
+		while True:
+			time.sleep(10.0)
 
-                for row in cur.fetchone():
-                        metricdata = self.hostname+"."+self.name+":"+str(row)+"|c"
-                        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-                        sock.sendto(metricdata, (self.statsd_host, int(self.statsd_port)))
-                        print metricdata
-                time.sleep(float(self.rate))
-
-# First we open the config files and set up the Worker threads
+class Logger(threading.Thread):
 
 
-class Collector():
-        def __init__(self):
-                self.stdin_path = '/dev/null'
-                self.stdout_path = '/var/log/mysqls_statsd_collector.log'
-                self.stderr_path = '/dev/tty'
-                self.pidfile_path = '/var/log/mysql_statsd_collector.pid'
-                self.pidfile_timeout = 5
+	def __init__(self, debug):
+		self.msgqueue = Queue.Queue()
+		self.debug = debug
+		super(Logger, self).__init__()
 
-        def ConfigSectionMap(self, section):
-                dict1 = {}
-                options = Cfg.options(section)
-                for option in options:
-                        try:
-                                dict1[option] = Cfg.get(section, option)
-                                if dict1[option] == -1:
-                                        print("skip: %s" % option)
-                        except:
-                                print("exception on %s!" % option)
-                                dict1[option] = None
-                return dict1
+	def log(self, message):
+		now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		fullmsg = now + " - " + message + "\n"
+		self.msgqueue.put(fullmsg)
 
-        def run(self):
-                sections = Cfg.sections()
-                mysql_host = self.ConfigSectionMap("config")['mysql_host']
-                mysql_user = self.ConfigSectionMap('config')['mysql_user']
-                statsd_host = self.ConfigSectionMap('config')['statsd_host']
-                statsd_port = self.ConfigSectionMap('config')['statsd_port']
-                mysql_password = self.ConfigSectionMap('config')['mysql_password']
-                sections.remove('config')
-                workers = []
-                for section in sections:
-                        rate = self.ConfigSectionMap(section)['rate']
-                        sql = self.ConfigSectionMap(section)['sql']
-                        worker = Worker(mysql_host, mysql_user, mysql_password, statsd_host, statsd_port, section, rate, sql)
-                        workers.append(worker)
-                for worker in workers:
-                        worker.daemon = True
-                        worker.start()
-                while True:
-                        time.sleep(10)
+	def debuglog(self, message):
+		if self.debug == "1":
+			now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                	fullmsg = now + " - DEBUG: " + message + "\n"
+                	self.msgqueue.put(fullmsg)
 
-Cfg = ConfigParser.ConfigParser()
-Cfg.read('/usr/local/bin/mysql_statsd_collector.cfg')  # YEAH THIS IS UGLY - need to fix
-collector = Collector()
-daemon_runner = runner.DaemonRunner(collector)
-daemon_runner.do_action()
+	def run(self):
+		while True:
+			msg = self.msgqueue.get()
+			sys.stdout.write(msg)
+			self.msgqueue.task_done()
+
+
+mambo = Mambo()
+mambo_runner = runner.DaemonRunner(mambo)
+mambo_runner.do_action()
